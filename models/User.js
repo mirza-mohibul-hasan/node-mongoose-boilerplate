38e6c538b509a517fd0838082fa17f85cc135989
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2025-03-08 06:00:56 +0600

    feat(model): add User model with basic schema (name, email, password)

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // The unique property is used to ensure that no two documents in the collection have the same value for the email field.
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
