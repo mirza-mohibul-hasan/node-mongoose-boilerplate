260ff7304084bdc92a53f90ee0bc7a3769e28201
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2025-03-09 21:50:49 +0600

    chore(db): implement MongoDB connection logic with retry and timeouts

--- config/db.js
+++ config/db.js
@@ -4,20 +4,21 @@ require("dotenv").config();
 const MONGO_URI = process.env.DB_URL;
 const clientOptions = {
   serverApi: { version: "1", strict: true, deprecationErrors: true },
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
 };
+
 async function connectDB() {
   try {
     await mongoose.connect(MONGO_URI, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
     process.exit(1);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
   }
 }
 
